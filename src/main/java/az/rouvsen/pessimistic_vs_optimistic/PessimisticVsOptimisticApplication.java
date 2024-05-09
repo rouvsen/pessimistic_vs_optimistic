e62ab436f5b7614fe66c777dba1d8a6ad5f6f6fb
rouvsen <rouvsen@gmail.com>
2024-05-09 17:36:35 +0400

    initial commit

---
+++ src/main/java/az/rouvsen/pessimistic_vs_optimistic/PessimisticVsOptimisticApplication.java
@@ -0,0 +1,14 @@
+package az.rouvsen.pessimistic_vs_optimistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessimisticVsOptimisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PessimisticVsOptimisticApplication.class, args);
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/test/java/az/rouvsen/pessimistic_vs_optimistic/PessimisticVsOptimisticApplicationTests.java
@@ -0,0 +1,13 @@
+package az.rouvsen.pessimistic_vs_optimistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessimisticVsOptimisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
